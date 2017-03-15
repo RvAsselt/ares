b234fadecfc5905c093c16d873ee6bf91a2c0028
RvAsselt <r.van.asselt@hotmail.com>
2017-03-15 13:43:45 +0100

    bijna

---
+++ server/stats.go
@@ -0,0 +1,48 @@
+package server
+
+import (
+	"net/http"
+
+	web "github.com/dutchcoders/ares-web"
+
+	"fmt"
+	"github.com/elazarl/go-bindata-assetfs"
+	"github.com/fatih/color"
+	// logging "github.com/op/go-logging"
+	"os"
+)
+
+func (hc *Server) startStatsServer() {
+	log.Infof("Stats server Listening on port: %s", hc.config.Web.Port)
+
+	staticHandler := http.FileServer(
+		&assetfs.AssetFS{
+			Asset:    web.Asset,
+			AssetDir: web.AssetDir,
+			AssetInfo: func(path string) (os.FileInfo, error) {
+				return os.Stat(path)
+			},
+			Prefix: web.Prefix,
+		})
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/", staticHandler)
+
+	if hc.config.Web.Path != "" {
+		log.Debug("Using static file path: ", hc.config.Web.Path)
+
+		// check local css first
+		staticHandler = http.FileServer(http.Dir(hc.config.Web.Path))
+	}
+
+	fmt.Println(color.YellowString(fmt.Sprintf("Honeytrap server started, listening on address %s.", hc.config.Web.Port)))
+
+	defer func() {
+		fmt.Println(color.YellowString(fmt.Sprintf("Honeytrap server stopped.")))
+	}()
+
+	if err := http.ListenAndServe(hc.config.Web.Port, mux); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
